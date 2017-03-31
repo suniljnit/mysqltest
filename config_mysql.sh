8157021049b56e982cea774be065835db9786716
suniljnit <k0803.sunil@gmail.com>
2017-03-31 12:28:02 -0500

    Update config_mysql.sh

--- config_mysql.sh
+++ config_mysql.sh
@@ -6,9 +6,9 @@ echo "Running the mysql_config function."
 yum -y erase mysql mysql-server
 rm -rf /var/lib/mysql/ /etc/my.cnf
 yum -y install mysql mysql-server
-mysql_install_db
+mysql_install_db --user=mysql
 chown -R mysql:mysql /var/lib/mysql
-/usr/bin/mysqld_safe & 
+/usr/bin/mysqld_safe --user=mysql & 
 sleep 10
 }
 
