912fef214410894566544923227b5bf42da5849a
suniljnit <k0803.sunil@gmail.com>
2017-03-31 15:31:42 -0500

    Update config_mysql.sh

--- config_mysql.sh
+++ config_mysql.sh
@@ -3,14 +3,14 @@
 __mysql_config() {
 # Hack to get MySQL up and running... I need to look into it more.
 echo "Running the mysql_config function."
-yum -y erase mysql mysql-server
-rm -rf /var/lib/mysql/ /etc/my.cnf
+#yum -y erase mysql mysql-server
+#rm -rf /var/lib/mysql/ /etc/my.cnf
 wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 rpm -ivh mysql57-community-release-el7-9.noarch.rpm
 yum install mysql-server -y
 systemctl start mysqld
 grep 'temporary password' /var/log/mysqld.log
-sleep 10
+sleep 5
 }
 
 #__start_mysql() {
