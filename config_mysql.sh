ad178d77b0804fc84d19d3c217cab44bc8c1192a
suniljnit <k0803.sunil@gmail.com>
2017-03-31 14:46:01 -0500

    Update config_mysql.sh

--- config_mysql.sh
+++ config_mysql.sh
@@ -9,7 +9,7 @@ wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 rpm -ivh mysql57-community-release-el7-9.noarch.rpm
 yum install mysql-server -y
 systemctl start mysqld
-#grep 'temporary password' /var/log/mysqld.log
+grep 'temporary password' /var/log/mysqld.log
 sleep 10
 }
 
